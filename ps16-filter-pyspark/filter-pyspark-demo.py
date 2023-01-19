dd9eb66e941e58ab18d6a568e4292af99688a668
Rehan Dominic <56755435+rehandominic@users.noreply.github.com>
2023-01-19 13:22:13 +0530

    Create filter-pyspark-demo.py

---
+++ ps16-filter-pyspark/filter-pyspark-demo.py
@@ -0,0 +1,37 @@
+df2 = spark.read.format("csv").option("header","true").load("dbfs:/FileStore/csv/mockdata.csv")
+
+df2.printSchema()
+
+#root
+# |-- id: integer (nullable = true)
+# |-- first_name: string (nullable = true)
+# |-- last_name: string (nullable = true)
+# |-- email: string (nullable = true)
+# |-- gender: string (nullable = true)
+# |-- ip_address: string (nullable = true)
+
+# Return Dataframe data where Gender is Male
+
+display(df2.filter(df.gender == "Male"))
+
+# Return Dataframe data where ID is greater than 20
+
+display(df.filter(df.id >20))
+
+# !!NOTE!! Use '&' for 'and' , '|' for 'or' , '~' for 'not' 
+
+# Return Dataframe data where ID is greater than 20 AND Gender is Female
+
+display(df.filter((df.id >20) & (df.gender == "Female")))
+
+# Return Dataframe data where ID is greater than 20 OR Gender is Female
+
+display(df.filter((df.id >20) | (df.gender == "Female")))
+
+# Return Dataframe data where ID is NOT greater than 20
+
+display(df.filter(~(df.id >20)))
+
+# Return Dataframe data where ID is greater than 20 AND Gender is NOT Female
+
+display(df.filter((df.id >20) & ~(df.gender == "Female")))
